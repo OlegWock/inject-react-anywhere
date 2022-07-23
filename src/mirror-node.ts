4317e663a5c1d73ba259891ecfd22e01a1fd0705
OlegWock <olegwock@gmail.com>
2022-07-23 20:33:51 +0200

    Fix bug when mirroring doesn't work if provided both nodes and source did not change over time

--- src/mirror-node.ts
+++ src/mirror-node.ts
@@ -1,5 +1,12 @@
 export const createMirror = <T extends Element>(source: T, destination?: T) => {
-    const mirror = destination || source.cloneNode(true) as T;
+    let mirror: T;
+    if (destination) {
+        mirror = destination;
+        mirror.innerHTML = source.innerHTML;
+        [...source.attributes].forEach(attr => { mirror.setAttribute(attr.nodeName, attr.nodeValue!) });
+    } else {
+        mirror = source.cloneNode(true) as T;
+    }
     const observer = new MutationObserver((changes) => {
         changes.forEach(ch => {
             if (ch.type === 'attributes' && ch.target === source) {
@@ -16,7 +23,7 @@ export const createMirror = <T extends Element>(source: T, destination?: T) => {
     });
 
     return {
-        mirror, 
+        mirror,
         stop: () => observer.disconnect(),
         resume: () => observer.observe(source, {
             subtree: true,
