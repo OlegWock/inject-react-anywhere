f0415f350d7e52eddfd3d34d90c0016aafdc24b4
OlegWock <olegwock@gmail.com>
2023-09-06 11:33:19 +0200

    Expose useConnectedPortalsEffect

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export type { InjectionResult } from './inject.js';
 export { createInjectableComponent, stringStyles, remoteStyles, combineStyleInjectors } from './wrapper.js';
 export { injectComponent, createShadowPortal } from './inject.js';
 export { ShadowPortal } from './types.js';
-export { useShadowDom } from './hooks.js';
+export { useShadowDom, useConnectedPortalsEffect } from './hooks.js';
 export { withShadowDom } from './hoc.js';
